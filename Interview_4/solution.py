2b6b023554e0d59075de940657d7a0ffe7f56d0a
gustavoeso <gustavo.eso@outlook.com.br>
2025-03-12 11:22:49 -0300

    interview 4

---
+++ Interview_4/solution.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, value=None, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+def in_order_succ(node: TreeNode) -> TreeNode:
+    if node is None:
+        return None
+
+    # Caso 1: se tem uma subárvore à direita, desce nela e pega o nó mais à esquerda
+    if node.right:
+        ref = node.right
+        while ref.left:
+            ref = ref.left
+        return ref
+
+    # Caso 2: Não existe subárvore à direita, então subir na árvore até encontrar um nó que seja filho à esquerda
+    atual = node
+    pai = node.parent
+    while pai and pai.right == atual:
+        atual = pai
+        pai = pai.parent
+
+    return pai
